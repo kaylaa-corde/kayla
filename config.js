28884f1ad739e86e0d5f9422ab72f6814a7240cd
kaylaa-corde <116987386+kaylaa-corde@users.noreply.github.com>
2022-11-03 05:58:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
 global.linkGC = [] // link gc lu
 global.channelYT = [] // link yt lu klo g ad g ush di isi
-global.owner = ['628818770766'] // masuk in no lu sebagai owner
-global.mods = ['6283897390164'] // masuk in no moderator ny
+global.owner = ['6285939048622'] // masuk in no lu sebagai owner
+global.mods = ['6285939048622'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -32,13 +32,13 @@ global.APIKeys = { // Apikey lu di sini
 }
 
 // Sticker Watermark
-global.packname = 'NayLaBotz'
-global.author = 'Zevano'
+global.packname = 'KayyBotz'
+global.author = 'kaylaa'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
-global.botwm = ['© nayla'] // watermark bot
-global.oname = ['Zevano'] // nama owner nya
+global.botwm = ['© Kayybotz'] // watermark bot
+global.oname = ['Kaylaa'] // nama owner nya
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
